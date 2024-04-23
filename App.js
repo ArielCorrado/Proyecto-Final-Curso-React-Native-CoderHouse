3d52b5863d60026b3a6f288b3b70a6572efe1061
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-04-23 14:26:14 -0300

    antes de empezar con navigation

--- App.js
+++ App.js
@@ -1,45 +1,36 @@
-import { StyleSheet, View, StatusBar } from 'react-native';
+import { StyleSheet, SafeAreaView, Platform } from 'react-native';
 import { useFonts } from 'expo-font';
-import { useEffect} from 'react';
 import Home from './src/screens/Home';
-import { MAIN_PADDING } from './src/constants/dimensions';
-import * as SplashScreen from 'expo-splash-screen';
+import { MAIN_PADDING, STATUS_BAR_HEIGHT } from './src/constants/dimensions';
+
 
 export default function App() {
 
     const [fontsLoaded, fontError] = useFonts({
         "Poppins": require("./assets/fonts/Poppins-Regular.ttf"),
     })
-
-    useEffect(() => {
-        const onLayoutRootView = async () => {
-            if (fontsLoaded || fontError) {
-                await SplashScreen.hideAsync();
-            }
-        };
-        onLayoutRootView();
-    }, [fontsLoaded, fontError]);
-    
+        
     if (!fontsLoaded || fontError) {
         return null;
     }
     
-    return (
-        <View style={styles.container}>
-            <StatusBar/>
-            <Home/>    
-        </View>
-    );
+    if (fontsLoaded && !fontError) {
+        return (
+            <SafeAreaView style={styles.container}>
+                <Home/>    
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
-        display: "flex",
         flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'center',
         width: "100%",
         backgroundColor: 'white',
         padding: MAIN_PADDING,
+        marginTop: Platform.OS === 'android' ? STATUS_BAR_HEIGHT : 0,
     },
 });
